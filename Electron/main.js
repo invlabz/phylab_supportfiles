a496c3a3aff786c19d6e5b892b8ece05400ca7e2
Patan Amrulla Khan <pamrulla@gmail.com>
2020-11-10 08:18:49 +0530

    Adding electron style

---
+++ Electron/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron')
+
+if (require('electron-squirrel-startup')) return app.quit();
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: './Iconlogo.png',
+    webPreferences: {
+      nodeIntegration: false,
+	    devTools: true
+    }
+  })
+  win.maximize()
+  // win.setMenu(null)
+  win.setMaximizable(false);
+
+  win.loadFile('index.html')
+//   win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
